283051275ac3782e9ea99b6bf44355817c5043b8
Robin Drexler <drexler.robin@gmail.com>
2013-11-09 22:54:59 +0100

    add feature, without any error handling whatsoever (as required :D)

---
+++ requireGist.js
@@ -0,0 +1,33 @@
+function requireGist(id, requestedFileNames, callback)
+{
+	function sent() {
+
+			var contents = {},
+				callbackArgs = [],
+				response = this.responseText;
+				json = JSON.parse(response);
+
+
+			for (var file in json.files) {
+				file = json.files[file];
+
+				if(requestedFileNames.indexOf(file.filename) > -1) {
+					contents[file.filename] = file.content
+				}
+			}
+			
+			requestedFileNames.forEach(function(name) {
+				callbackArgs.push(new Function(contents[name]));
+			});
+
+			callback.apply(window, callbackArgs);
+
+    }
+
+    xhr = new XMLHttpRequest();
+    xhr.onload = sent;
+    xhr.open("get", "https://api.github.com/gists/" + id, true);
+    xhr.setRequestHeader('Accept', 'application/vnd.github.v3.raw+json')
+    xhr.send();
+    
+}
