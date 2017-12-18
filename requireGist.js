51968bdf5b150fd2e9818ba144a4388d1f40c548
Robin Drexler <drexler.robin@gmail.com>
2017-12-18 23:28:04 +0100

    chore: prettier

--- requireGist.js
+++ requireGist.js
@@ -1,32 +1,27 @@
-function requireGist(id, requestedFileNames, callback)
-{
-	function sent() {
+function requireGist(id, requestedFileNames, callback) {
+  function sent() {
+    var contents = {},
+      callbackArgs = [],
+      response = this.responseText,
+      json = JSON.parse(response);
 
-			var contents = {},
-				callbackArgs = [],
-				response = this.responseText,
-				json = JSON.parse(response);
+    for (var file in json.files) {
+      file = json.files[file];
 
-
-			for (var file in json.files) {
-				file = json.files[file];
-
-				if(requestedFileNames.indexOf(file.filename) > -1) {
-					contents[file.filename] = file.content
-				}
-			}
-			
-			requestedFileNames.forEach(function(name) {
-				callbackArgs.push(new Function(contents[name]));
-			});
-
-			callback.apply(window, callbackArgs);
-
+      if (requestedFileNames.indexOf(file.filename) > -1) {
+        contents[file.filename] = file.content;
+      }
     }
 
-    xhr = new XMLHttpRequest();
-    xhr.onload = sent;
-    xhr.open("get", "https://api.github.com/gists/" + id, true);
-    xhr.send();
-    
+    requestedFileNames.forEach(function(name) {
+      callbackArgs.push(new Function(contents[name]));
+    });
+
+    callback.apply(window, callbackArgs);
+  }
+
+  xhr = new XMLHttpRequest();
+  xhr.onload = sent;
+  xhr.open('get', 'https://api.github.com/gists/' + id, true);
+  xhr.send();
 }
